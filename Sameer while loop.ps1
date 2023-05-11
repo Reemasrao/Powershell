4b8538c86c45aa4b37e0c2a23349867fe55ea51a
Reemasrao <132883707+Reemasrao@users.noreply.github.com>
2023-05-11 17:58:00 +0530

    Add files via upload

---
+++ Sameer while loop.ps1
@@ -0,0 +1,103 @@
+﻿
+
+1. $i=0
+while ($i -lt 10) {
+
+    $foldername = 'file' + $i
+    
+
+    
+    New-Item -path $foldername -ItemType file
+
+     
+     $i = $i + 1
+     }
+
+
+
+
+
+2. $i=0
+while ($i -lt 10) {
+
+    $foldername = 'folder' + $i +'\'
+    $fname = sameer
+
+    
+    New-Item -path $foldername -ItemType Directory
+
+     
+     $i = $i + 1
+
+     }
+
+
+3.Create 10 folders inside the folders created in step 2
+$i=0
+while($i -lt 10)
+{
+$j=0;
+while($j -lt 10)
+{
+New-Item -Path folder$i/sameer$j -ItemType Directory;
+$j++;
+}
+$i++;
+}
+
+4.Create 10 files in the 10 folders created in step 3
+
+$i=0
+while($i -lt 10)
+{
+$j=0;
+while($j -lt 10)
+{
+$k=0;
+while($k -lt 10){
+New-Item -Path folder$i/sameer$j/new$k.txt -ItemType File;
+$k++;
+}
+$j++;
+}
+$i++;
+}
+
+
+5.Copy 10 files to each of 10 folders
+$i=0
+while($i -lt 10)
+{
+$j=0;
+while($j -lt 10)
+{
+$k=0;
+while($k -lt 10){
+Copy-Item Textfile$i.txt  folder$j/sameer$k;
+$k++;
+}
+$j++;
+}
+$i++;
+}
+
+
+6.Remove all the files from the folders
+$i=0
+while($i -lt 10)
+{
+$j=0;
+while($j -lt 10)
+{
+$k=0;
+while($k -lt 10){
+Remove-Item folder$i/sameer$j/*.txt;
+$k++;
+}
+$j++;
+}
+$i++;
+}
+
+     
+    
---
+++ Sameerb for loop.ps1
@@ -0,0 +1,59 @@
+﻿
+
+1. for ($i = 1 ; $i -lt 11 ; $i++) {
+    $filename = 'file' + $i + '.txt'
+    
+    New-Item -Path $filename -ItemType File
+
+    }
+     
+
+2. for ($i = 1 ; $i -lt 11 ; $i++) 
+{
+for ($j = 1 ; $j -lt 11 ; $j++) {
+    
+    
+    
+    New-Item -Path "folder$i\sameer$j" -ItemType Directory
+
+   }
+   }
+
+
+3.for ($i = 1 ;  $i -lt 11; $i++) {
+
+    for($a = 1 ; $a -lt 11 ; $a++) {
+
+        for($b = 1 ; $b -lt 11 ; $b++) {
+
+        New-Item -Path "folder$i\sameer$a\text$b.txt" -ItemType file
+
+        }
+        }
+        }
+
+4.for ($i = 1 ;  $i -lt 11; $i++) {
+
+    for($a = 1 ; $a -lt 11 ; $a++) {
+
+        $abc = 'file' + $a + '.txt'
+        $xyz = 'folder' + $i + 'sameer' +  $a
+
+        Copy-Item -Path "$abc" "$xyz"
+
+        }
+        }
+        
+
+5.for ($i = 1 ;  $i -lt 11; $i++) {
+
+    for($a = 1 ; $a -lt 11 ; $a++) {
+      for($b = 1 ; $b -lt 11 ; $b++) {
+
+        $abc = 'file' + $b + '.txt'
+
+        Copy-Item -Path "$abc" "folder$i\sameer$a"
+
+        }
+        }
+        }
